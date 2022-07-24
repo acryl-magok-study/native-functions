c78bc6a1c0b18d189753462dd0a3c7a5905f035b
silver j <janggoon235@gmail.com>
2022-07-25 00:39:28 +0900

    firstcommit

---
+++ native-functions/native_functionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_functionsApp.swift
+//  native-functions
+//
+//  Created by silver j on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct native_functionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
